7bb7bab65cf831a4bf6be8754b55593118d38778
Vadim Eremichev <ve@anahoret.com>
2016-06-08 15:19:30 +0300

    finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,44 @@ require 'rails_helper'
 
 describe "Static pages" do
   describe "Home page" do
-    it "shold ahve content 'Sample App'" do
+    before do
       visit '/static_pages/home'
+    end
+
+    it "shold have content 'Sample App'" do
       expect(page).to have_content('Sample App')
+    end
+
+    it "should have the title 'Home'" do
+      expect(page).to have_title("Home")
+    end
+  end
+
+  describe "Help page" do
+    before do
+      visit '/static_pages/help'
+    end
+
+    it "shoud have content 'Help'" do
+      expect(page).to have_content("Help")
+    end
+
+    it "should have the title 'Help'" do
+      expect(page).to have_title("Help")
+    end
+  end
+
+  describe "About page" do
+    before do
+      visit '/static_pages/about'
+    end
+
+    it "should have content 'About'" do
+      expect(page).to have_content("About")
+    end
+
+    it "should have the title 'About'" do
+      expect(page).to have_title("About")
     end
   end
 end
