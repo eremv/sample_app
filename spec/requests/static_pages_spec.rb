4b0fad60b8ac137b17c856296cfe2cfaf13be093
Vadim Eremichev <ve@anahoret.com>
2016-06-08 11:54:01 +0300

    tune capybara rspec up

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+    it "shold ahve content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+end
