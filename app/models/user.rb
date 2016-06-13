5b5041aa3d08ac53be4c7c8fd60e61f9f3db7e3b
Vadim Eremichev <ve@anahoret.com>
2016-06-13 19:19:58 +0300

    users validation

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  validates :name, presence: true, length: {maximum: 50, minimum: 3}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 50},
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false}
+  before_save { self.email = email.downcase}
+  validates :password , presence: true
+  has_secure_password
+end
