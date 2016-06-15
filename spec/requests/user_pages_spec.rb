1fa7243053a6b15a58662d64c9d26a231e8f50b6
Vadim Eremichev <ve@anahoret.com>
2016-06-15 18:34:15 +0300

    add registation form with errors

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,8 +6,31 @@ describe 'user pages' do
   describe 'signup page' do
     before { visit signup_path }
 
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create new user" do
+        expect { click_button submit}.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name", with: "John"
+        fill_in "Email", with: "jd@ve.anahoret.com"
+        fill_in "Password", with: "password"
+        fill_in "Confirmation", with: "password"
+
+      end
+
+      it "should create new user" do
+        expect{ click_button submit}.to change(User, :count).by(1)
+      end
+    end
+
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
+
   end
 
   describe 'profile page' do
